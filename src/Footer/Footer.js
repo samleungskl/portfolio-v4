a8366f4deaf4308c80f92209b8375055cf2a9653
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-12 07:01:13 -0700

    add hidden span for accessibility

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
         <div className="Footer">
             <section className='footerSection'>
                 <div className='iconContainer'>
-                    <a className="linkedInIcon fa-brands fa-linkedin icon" href='https://www.linkedin.com/in/samleungskl/'></a>
-                    <a className="envelopeIcon fa-solid fa-envelope icon" href='mailto: samleungskl@gmail.com'></a>
+                    <a className="linkedInIcon fa-brands fa-linkedin icon" href='https://www.linkedin.com/in/samleungskl/'><span className='screenReader'>LinkedIN</span></a>
+                    <a className="envelopeIcon fa-solid fa-envelope icon" href='mailto: samleungskl@gmail.com'><span className='screenReader'>E-mail</span></a>
                 </div>
                 <div className='designByMsg'>Designed & Built by Sam Leung</div>
                 <div className='designYear'>2022</div>
