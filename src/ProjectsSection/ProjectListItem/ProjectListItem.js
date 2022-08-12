fa7d77f27848204745ddb4f0b9dfa58617657753
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-12 11:48:05 -0700

    fix missing hover code

--- src/ProjectsSection/ProjectListItem/ProjectListItem.js
+++ src/ProjectsSection/ProjectListItem/ProjectListItem.js
@@ -5,15 +5,16 @@ function ProjectListItem({ projectName, projectDescription, projectTechStack, pr
     const cssContainerClassName = `${projectNameNoSpace}Container`
     const cssProjectNameClassName = `${projectNameNoSpace}Name`
     const cssClassName = `${projectNameNoSpace}Image project`
+    const cssInfoContainer = `${projectNameNoSpace}InfoContainer infoContainer`
     return (
-        <div className='ProjectListItemContainer' onClick={() => {changeProjectVisible(projectRecID, 'click') }} 
+        <div className='ProjectListItemContainer' onClick={() => {changeProjectVisible(projectRecID, 'click') }} onMouseEnter={() => {changeProjectVisible(projectRecID, 'hoverIn') }} onMouseLeave={() => {changeProjectVisible(projectRecID, 'hoverOut') }} 
         >
             <div className={cssContainerClassName}>
                 <div style={cssClassBackgroundImg} className={cssClassName}>
                     <div className={cssProjectNameClassName}>{projectName}</div>
                 </div>
             </div>
-            <div className='infoContainer' style={{ opacity: projectInfoHide ? 0 : 1 }}>
+            <div className={cssInfoContainer} style={{ opacity: projectInfoHide ? 0 : 1 }}>
                 <div className='infoTitle'>{projectName}</div>
                 <div className='infoDescription'>{projectDescription}</div>
                 <div className='infoIconContainer'>
