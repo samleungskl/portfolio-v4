d9cc3f740bc9737429df6ab90ce2fa501d5e9ab4
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-12 19:43:08 -0700

    fixed background pattern on welcome section

--- src/WelcomSection/WelcomeSection.js
+++ src/WelcomSection/WelcomeSection.js
@@ -3,10 +3,12 @@ import './WelcomeSection.css';
 function WelcomeSection() {
     return (
         <div className='welcomeSectionBackground'>
-            <div className='WelcomeSection marginLeftRight'>
-                <div className='welcomeTitle title'> Hi! I am Sam.</div>
-                <div className='welcomeMsg'> Welcome to my website.</div>
-                <div className='explainMsg msg'> I like front-end things and back-end things, so I decided to become a full stack developer.</div>
+            <div className='welcomeSectionPattern'>
+                <div className='WelcomeSection marginLeftRight'>
+                    <div className='welcomeTitle title'> Hi! I am Sam.</div>
+                    <div className='welcomeMsg'> Welcome to my website.</div>
+                    <div className='explainMsg msg'> I like front-end things and back-end things, so I decided to become a full stack developer.</div>
+                </div>
             </div>
         </div>
     );
