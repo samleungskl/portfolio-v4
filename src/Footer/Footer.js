553753082257841cbab9f7f38028c64bc035ad4b
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-13 11:59:47 -0700

    adjust footer padding

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -8,8 +8,10 @@ function Footer() {
                     <a className="linkedInIcon fa-brands fa-linkedin icon" href='https://www.linkedin.com/in/samleungskl/'><span className='screenReader'>LinkedIN</span></a>
                     <a className="envelopeIcon fa-solid fa-envelope icon" href='mailto: samleungskl@gmail.com'><span className='screenReader'>E-mail</span></a>
                 </div>
+                <div className='footerTextContainer'>
                 <div className='designByMsg'>Designed & Built by Sam Leung</div>
                 <div className='designYear'>2022</div>
+                </div>
             </section>
         </div>
     );
