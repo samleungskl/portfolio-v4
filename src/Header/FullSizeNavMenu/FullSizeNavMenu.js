c01498da4ab0ee671fb1d4627ab04adb5897985b
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-14 03:23:31 -0700

    adjust nav text

--- src/Header/FullSizeNavMenu/FullSizeNavMenu.js
+++ src/Header/FullSizeNavMenu/FullSizeNavMenu.js
@@ -4,8 +4,8 @@ import MessageButton from '../../MessageButton/MessageButton';
 function FullSizeNavMenu() {
     return (
         <div className="FullSizeNavMenu">
-            <a className='navAboutMe navItem' href='#about-me'>About Me</a>
-            <a className='navMyProjects navItem' href='#projects'>My Projects</a>
+            <a className='navAboutMe navItem' href='#about-me'>About</a>
+            <a className='navMyProjects navItem' href='#projects'>Projects</a>
             <a className='navMyContact navItem' href='#contact'>Contact</a>
         </div>
     );
